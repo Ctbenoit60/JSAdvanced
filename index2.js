3f3b39aa595b57098317f72e953af957e1d3cbd8
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-06 19:35:29 -0600

    removed second call

--- index2.js
+++ index2.js
@@ -21,7 +21,7 @@ const delayMsg = (msg) => console.log(`This message will be printed after a dela
 
 let timeOut = setTimeout(delayMsg, 10000, '#5: Delayed by 10 seconds or 10000 ms')
 
-setTimeout(delayMsg, 10000, '#5: Delayed by 10 seconds or 10000 ms')
+// setTimeout(delayMsg, 10000, '#5: Delayed by 10 seconds or 10000 ms')
 setTimeout(delayMsg, 100, '#1: Delayed by 100ms');
 setTimeout(delayMsg, 20, '#2: Delayed by 20ms');
 setTimeout(delayMsg, 0, '#3: Delayed by 0ms');
