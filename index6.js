be40f8df54381ab39dfa23b1dea5cbc1edbffb9b
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-10 16:43:07 -0600

    part a

--- index6.js
+++ index6.js
@@ -1,9 +1,14 @@
 // Use the Function prototype to add a new delay(ms) function to all functions, which can
 // be used to delay the call to that function by ms milliseconds.
 function multiply(a, b) {
-console.log( a * b );
-}
-multiply.delay(500)(5, 5); // prints 25 after 500 milliseconds
+    setTimeout(function () {
+      console.log(a * b);
+    }, 500);
+  }
+  
+  multiply(5, 5);
+   // prints 25 after 500 milliseconds
+
 // a) Use the example multiply function below to test it with, as above, and assume that all
 // delayed functions will take two parameters
 // b) Use apply to improve your solution so that delayed functions can take any number of
