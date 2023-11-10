4b4e0ac90fd21fa82d80834432b8f85749c977fe
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-10 15:44:33 -0600

    part c

--- index5.js
+++ index5.js
@@ -23,8 +23,16 @@ setTimeout(wrap(), 200);
 // function
 
 // b) Change the year for the car by creating a clone of the original and overriding it
+
 // c) Does the delayed description() call use the original values or the new values from
 // b)? Why?
+// The delayed description() call would still use the original values. 
+// This is because the description method is not part of the cloned object. 
+// The method is defined in the original car object and references the this context, 
+// which remains bound to the original object. Even if you update the properties in the cloned object, 
+// the method still references the original object's properties.
+
+
 // d) Use bind to fix the description method so that it can be called from within
 // setTimeout without a wrapper function
 // e) Change another property of the car by creating a clone and overriding it, and test that
