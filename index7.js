f7fd18dcf93dc03823d965fcd8311bf7ed9f3f1a
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-10 18:40:45 -0600

    answered part a,b,c & d

--- index7.js
+++ index7.js
@@ -5,17 +5,45 @@
 // meaningful string representation.
 // a) Define a custom toString method for the Person object that will format and print
 // their details
+
+Person.prototype.toString = function () {
+    return `${this.name}, is a ${this.age}, years old, ${this.gender}`
+};
 // b) Test your method by creating 2 different people using the below constructor function
 // and printing them
-// c) Create a new constructor function Student that uses call to inherit from Person and
-// add an extra property cohort
 
-// d) Add a custom toString for Student objects that formats and prints their details. Test
-// with 2 students.
+const person2 = new Person('Corey Brown', 30, 'male')
+
+const person3 = new Person('Christina Brown', 28, 'female')
+
 function Person(name, age, gender) {
 this.name = name;
 this.age = age;
 this.gender = gender;
 }
+
 const person1 = new Person('James Brown', 73, 'male')
 console.log('person1: '+person1) //prints person1: [object Object]
+console.log('person2: '+person2)
+
+
+// c) Create a new constructor function Student that uses call to inherit from Person and
+// add an extra property cohort
+
+function Student(name, age, gender, cohort) {
+    Person.call(this, name, age, gender);
+    this.cohort = cohort;
+}
+
+// d) Add a custom toString for Student objects that formats and prints their details. Test
+// with 2 students.
+
+Student.prototype.toString = function() {
+    return `${Person.prototype.toString.call(this)}, Cohort: ${this.cohort}`;
+};
+
+const student1 = new Student('John Breaux', 20, 'male', '2023A');
+console.log('student1: ' + student1); 
+
+const student2 = new Student('Alice Hebert', 22, 'female', '2023B');
+console.log('student2: ' + student2); 
